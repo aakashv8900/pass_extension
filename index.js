ae3da5ebbbb44dc777c4fd6eabfa870ae28e10d7
Aakash Verma <aakashv.8292@gmail.com>
2021-12-11 20:25:01 +0530

    building

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const crypto = require("crypto")
+const http = require("http");
+const crypto = require("crypto");
+const path = require("path");
+
+
 const algorithm = "aes-256-cbc"
 const initVector = crypto.randomBytes(16)
 const Securitykey = crypto.randomBytes(32)
 const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector)
 const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector)
 
-let text = window.document.getElementById("text")
-let data = window.document.getElementById("data")
+
+const server = http.createServer((req, res) => {
+    if(req.url == "/"){
+        res.writeHead(200, {"Content-type":"text/html"});
+        res.sendFile(path.join(__dirname+'/index.html'));
+    }
+});
 
 
-let encryptedData = cipher.update(text, "utf-8", "hex")
-encryptedData += cipher.final("hex")
+// let encryptedData = cipher.update(text, "utf-8", "hex")
+// encryptedData += cipher.final("hex")
 
-function encr(){
-    data.innerHTML =  encryptedData;
-    alert('Encryption')
-}
+// function encr(){
+//     data.innerHTML =  encryptedData;
+//     alert('Encryption')
+// }
 
-let decryptedData = decipher.update(encryptedData, "hex", "utf-8")
-decryptedData += decipher.final("utf8")
+// let decryptedData = decipher.update(encryptedData, "hex", "utf-8")
+// decryptedData += decipher.final("utf8")
 
 
-function decr(){
-    data.innerHTML = decryptedData;
-    alert('Decryption')
-}
+// function decr(){
+//     data.innerHTML = decryptedData;
+//     alert('Decryption')
+// }
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening");
+});
